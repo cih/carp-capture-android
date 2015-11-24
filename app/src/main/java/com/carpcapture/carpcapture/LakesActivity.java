1a53a8ff7ca490b5efdde577e6f184fd9bc75d23
Chris Holmes <tochrisholmes@gmail.com>
2015-11-24 13:41:34 +0000

    Add captures activity

--- app/src/main/java/com/carpcapture/carpcapture/LakesActivity.java
+++ app/src/main/java/com/carpcapture/carpcapture/LakesActivity.java
@@ -1,5 +1,6 @@
 package com.carpcapture.carpcapture;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class LakesActivity extends AppCompatActivity {
 
@@ -38,6 +40,13 @@ public class LakesActivity extends AppCompatActivity {
             return true;
         }
 
+        if (id == R.id.action_captures) {
+            Intent intent = new Intent(this, CapturesActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
+
     }
 }
