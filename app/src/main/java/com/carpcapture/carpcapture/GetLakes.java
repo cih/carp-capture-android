5f62a13a47cb0af96d8788a9bb1ae32cf3b7cd5e
Chris Holmes <tochrisholmes@gmail.com>
2015-11-25 11:21:04 +0000

    Add a basic list view

--- app/src/main/java/com/carpcapture/carpcapture/GetLakes.java
+++ app/src/main/java/com/carpcapture/carpcapture/GetLakes.java
@@ -6,9 +6,11 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
+import org.json.JSONObject;
 import org.json.simple.JSONValue;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Objects;
 
 /**
@@ -28,9 +30,9 @@ class GetLakes extends AsyncTask<String, Void, String> {
             Response response = client.newCall(request).execute();
             System.out.println(response);
             String responseBody = response.body().string();
-            Object obj = JSONValue.parse(responseBody);
+            HashMap obj = (HashMap) JSONValue.parse(responseBody);
             System.out.println(responseBody);
-            System.out.println(obj);
+            System.out.println(obj.get("lakes"));
             return responseBody;
         } catch (Exception e) {
             System.out.println(e);
--- app/src/main/java/com/carpcapture/carpcapture/LakesListActivity.java
+++ app/src/main/java/com/carpcapture/carpcapture/LakesListActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
@@ -21,6 +23,14 @@ public class LakesListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lakes_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        String names[] = {"Hadleigh Res","Blue Lagoon","Piggeries"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, names);
+
+        ListView listView = (ListView) findViewById(R.id.lakes_list);
+        listView.setAdapter(adapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
