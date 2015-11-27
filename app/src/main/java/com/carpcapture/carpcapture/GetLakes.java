a1460077b40adb9796a129986c99b94376f4e32e
Chris Holmes <tochrisholmes@gmail.com>
2015-11-27 17:59:47 +0000

    Simplify the lakes factory and pass the names to the list view

--- app/src/main/java/com/carpcapture/carpcapture/GetLakes.java
+++ app/src/main/java/com/carpcapture/carpcapture/GetLakes.java
@@ -17,7 +17,11 @@ import org.json.simple.JSONValue;
 
 import java.io.IOException;
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -25,6 +29,7 @@ import java.util.Objects;
  */
 class GetLakes extends AsyncTask<String, String, String> {
     private LakesListActivity lakeList;
+    List<String> lakeNames = new ArrayList<String>();
 
     public GetLakes(LakesListActivity lakesListActivity) {
        lakeList = lakesListActivity;
@@ -41,9 +46,7 @@ class GetLakes extends AsyncTask<String, String, String> {
         try {
             Response response = client.newCall(request).execute();
             String responseBody = response.body().string();
-            HashMap obj = (HashMap) JSONValue.parse(responseBody);
-
-            new LakesFactory(responseBody);
+//            HashMap obj = (HashMap) JSONValue.parse(responseBody);
 
             return responseBody;
         } catch (Exception e) {
@@ -57,10 +60,46 @@ class GetLakes extends AsyncTask<String, String, String> {
     protected void onPostExecute(String result) {
         super.onPostExecute(result);
 
+        LakesFactory lakes = new LakesFactory(result);
+        ArrayList lakesArray = lakes.convertToIterator();
+        Log.e("LAKESARRAY", lakesArray.toString());
+
+//        int i = 0;
+//        for ( ; lakesArray.hasNext() ; ++i ) lakesArray.next();
+
+        ArrayList<String> lakeNames = new ArrayList<String>();
+
+        for(int i = 0; i < lakesArray.size(); i++) {
+            Log.e("IN LAKE ARRAY LIST", lakesArray.get(i).toString());
+
+            HashMap lake = (HashMap) lakesArray.get(i);
+
+            lakeNames.add(lake.get("name").toString());
+        }
+
+//        while(lakesArray.hasNext()) {
+//            Map.Entry entry = (Map.Entry)lakesArray.next();
+//            HashMap lake = (HashMap)entry.getValue();
+//
+//            Log.e("LAKE", lake.toString());
+//
+//            String lakeName = (String)lake.get("name");
+//            lakeNames.add(lakeName);
+//        }
+//
+//        Log.e("LAKE NAMES", lakeNames.toString());
+
         String names[] = {"Hadleigh Res","Blue Lagoon","Piggeries"};
 
+        String [] newNames = lakeNames.toArray(new String[lakeNames.size()]);
+
+        Log.e("ORIGINAL LAKE NAMES", names.getClass().toString());
+        Log.e("ORIGINAL LAKE NAMES", names.toString());
+        Log.e("LAKE NAMES CLASS", newNames.getClass().toString());
+        Log.e("LAKE NAMES", newNames.toString());
+
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(lakeList,
-                R.layout.lake_list_item, R.id.textView1, names);
+                R.layout.lake_list_item, R.id.textView1, newNames);
 
         final ListView listView = (ListView) lakeList.findViewById(R.id.lakes_list);
         listView.setAdapter(adapter);
--- app/src/main/java/com/carpcapture/carpcapture/LakesFactory.java
+++ app/src/main/java/com/carpcapture/carpcapture/LakesFactory.java
@@ -6,7 +6,10 @@ import org.json.simple.JSONValue;
 import org.json.simple.parser.ContainerFactory;
 import org.json.simple.parser.JSONParser;
 
+import java.lang.reflect.Array;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
@@ -25,13 +28,23 @@ public class LakesFactory {
      *
      * */
 
-    public LakesFactory(String jsonText) {
-       // Pull out the lakes, iterate through passing everything to a LakeFactory
+    String jsonText;
+    ArrayList lakesArray;
+
+    public LakesFactory(String text) {
+        jsonText = text;
+
+        // Pull out the lakes, iterate through passing everything to a LakeFactory
+    }
+
+    public ArrayList convertToIterator() {
+        Log.e("JSON TEXT", jsonText);
+
         JSONParser parser = new JSONParser();
 
         ContainerFactory containerFactory = new ContainerFactory(){
             public List creatArrayContainer() {
-                return new LinkedList();
+                return new ArrayList();
             }
 
             public Map createObjectContainer() {
@@ -41,35 +54,39 @@ public class LakesFactory {
         };
 
 
-
-
-
         try {
             Map json = (Map)parser.parse(jsonText, containerFactory);
-            Iterator iter = json.entrySet().iterator();
-            Log.e("LakesFactory", "==iterate result==");
-            while(iter.hasNext()){
-                Map.Entry entry = (Map.Entry)iter.next();
-                Log.e("LakesFactory", entry.getKey() + "=>" + entry.getValue());
+            lakesArray = (ArrayList) json.get("lakes");
 
-                Log.e("LakesFactory", entry.getClass().toString()); // LinkedHashMap$LinkedEntry
-                Log.e("LakesFactory", entry.getValue().getClass().toString()); // LinkedList
+//            Iterator iter = json.entrySet().iterator();
+//            Log.e("LakesFactory", "==iterate result==");
+//            Log.e("ENTRY SET", json.entrySet().toString());
+//
+//
+//            while(iter.hasNext()){
+//                Map.Entry entry = (Map.Entry)iter.next();
+//                Log.e("LakesFactory", entry.getKey() + "=>" + entry.getValue());
+//
+//                Log.e("LakesFactory", entry.getClass().toString()); // LinkedHashMap$LinkedEntry
+//                Log.e("LakesFactory Value", entry.getValue().toString()); // LinkedList
+//
+//                LinkedList lakes = (LinkedList) entry.getValue();
+//
+//                lakesArray = lakes.listIterator();
+//
+//                while(lakesArray.hasNext()) {
+//                    Log.e("LakeFactory", lakesArray.next().toString());
+//                }
+//            }
 
-                LinkedList lakes = (LinkedList) entry.getValue();
-
-                Iterator lakesArray = lakes.listIterator();
-
-                while(lakesArray.hasNext()) {
-                    Log.e("LakeFactory", lakesArray.next().toString());
-                }
-            }
-
-            Log.e("LakesFactory", "==toJSONString()==");
-            Log.e("LakesFactory", JSONValue.toJSONString(json));
-
+//            Log.e("LakesFactory", "==toJSONString()==");
+//            Log.e("LakesFactory", JSONValue.toJSONString(json));
         }
         catch (org.json.simple.parser.ParseException e) {
             Log.e("LakesFactory", e.toString());
         }
+
+        return lakesArray;
     }
+
 }
