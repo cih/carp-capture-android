34c77a4af14cf6ed996ec05509608dec4c520372
Chris Holmes <tochrisholmes@gmail.com>
2015-12-04 15:24:47 +0000

    Show the selected image in capture view

--- app/src/main/java/com/carpcapture/carpcapture/CaptureActivity.java
+++ app/src/main/java/com/carpcapture/carpcapture/CaptureActivity.java
@@ -1,15 +1,23 @@
 package com.carpcapture.carpcapture;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
 
 public class CaptureActivity extends AppCompatActivity {
-    private static int RESULT_LOAD_IMG = 1;
+    private static final int RESULT_LOAD_IMG = 1;
+    private Bitmap bitmapImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +45,52 @@ public class CaptureActivity extends AppCompatActivity {
                 android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         // Start the Intent
         startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        switch (requestCode) {
+            case RESULT_LOAD_IMG:
+                if (resultCode == Activity.RESULT_OK) {
+
+
+                    Uri selectedImage = intent.getData();
+                    try {
+                        bitmapImage = decodeBitmap(selectedImage);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+
+                    // Show the Selected Image on ImageView
+                    ImageView imageView = (ImageView) findViewById(R.id.imgView);
+                    imageView.setImageBitmap(bitmapImage);
+                }
+        }
+    }
+
+    // http://stackoverflow.com/questions/21136311/picking-a-photo-from-gallery-and-show-in-a-image-view
+    public  Bitmap decodeBitmap(Uri selectedImage) throws FileNotFoundException {
+        BitmapFactory.Options o = new BitmapFactory.Options();
+        o.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(getContentResolver().openInputStream(selectedImage), null, o);
+
+        final int REQUIRED_SIZE = 100;
+
+        int width_tmp = o.outWidth, height_tmp = o.outHeight;
+        int scale = 1;
+        while (true) {
+            if (width_tmp / 2 < REQUIRED_SIZE || height_tmp / 2 < REQUIRED_SIZE) {
+                break;
+            }
+            width_tmp /= 2;
+            height_tmp /= 2;
+            scale *= 2;
+        }
+
+        BitmapFactory.Options o2 = new BitmapFactory.Options();
+        o2.inSampleSize = scale;
+        return BitmapFactory.decodeStream(getContentResolver().openInputStream(selectedImage), null, o2);
     }
 
 }
