18ec1a7a895da64d20aa07bacaed8c7016f31dad
Chris Holmes <tochrisholmes@gmail.com>
2016-01-10 20:06:36 +0000

    Fix orientation change crash

--- app/src/main/java/com/carpcapture/carpcapture/MenuActivity.java
+++ app/src/main/java/com/carpcapture/carpcapture/MenuActivity.java
@@ -1,9 +1,7 @@
 package com.carpcapture.carpcapture;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
